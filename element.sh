b4dcd405055e9699a56baeabe32f9e9542c8a896
nicolas potrich <nicolaspotrich3@gmail.com>
2024-08-17 17:00:49 +0000

    fix: element.sh refaccioned

--- element.sh
+++ element.sh
@@ -5,7 +5,13 @@ then
   echo "Please provide an element as an argument."
 else
   # Consultar la base de datos
-  RESULT=$($PSQL "SELECT atomic_number, symbol, name, atomic_mass, melting_point_celsius, boiling_point_celsius, type FROM elements INNER JOIN properties USING (atomic_number) INNER JOIN types USING (type_id) WHERE atomic_number::TEXT = '$1' OR symbol = '$1' OR name = '$1';")
+  RESULT=$(
+            $PSQL "SELECT atomic_number, symbol, name, atomic_mass, melting_point_celsius, boiling_point_celsius, type 
+                   FROM elements 
+                   INNER JOIN properties USING (atomic_number) 
+                   INNER JOIN types USING (type_id) 
+                   WHERE atomic_number::TEXT = '$1' OR symbol = '$1' OR name = '$1';"
+            )
 
   # Mostrar los resultados
   if [[ -z $RESULT ]]; then
